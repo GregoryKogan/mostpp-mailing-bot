7866d02089cad8d1264baf3adfc92b843d0b3d0c
GregoryKogan <grisha.koganovskiy@gmail.com>
2024-02-07 13:27:18 +0300

    Update handlers.py with localized messages

--- src/handlers.py
+++ src/handlers.py
@@ -16,11 +16,28 @@ from Cache import Cache
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user = update.effective_user
-    await update.message.reply_html(rf"Hi {user.mention_html()}!")
+    await update.message.reply_html(
+        f"Здравствуйте, {user.full_name}!\n"
+        "Это бот для управления регистрациями на мероприятия.\n"
+        "Для получения справки используйте команду /help."
+    )
 
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text("Help!")
+    await update.message.reply_text(
+        "<b>Для этого бота доступны следующие команды:</b>\n"
+        "/start - начать работу с ботом\n"
+        "/help - получить справку\n"
+        "/registrations - просмотреть регистрации на мероприятия\n\n"
+        "<b>Конфигурация:</b>\n"
+        f" - Рабочий email: \n{os.environ.get('EMAIL_ADDRESS')}\n"
+        f" - Email уведомителя: \n{os.environ.get('NOTIFIER_ADDRESS')}\n"
+        f" - Максимальное время регистрации: \n{config.REGISTRATION_PERIOD} мес.\n"
+        f" - Ключевые слова в теме письма: \n<code>{', '.join(config.REGISTRATION_SUBJECT_KEYWORDS)}</code>\n"
+        f" - Время жизни кеша регистраций: \n{config.REGISTRATIONS_CACHE_LIFETIME} сек.\n\n"
+        "Тех. поддержка: @GregoryKogan\n",
+        parse_mode=constants.ParseMode.HTML,
+    )
 
 
 async def meaningless(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
